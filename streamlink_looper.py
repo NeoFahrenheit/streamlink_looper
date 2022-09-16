ea77e2063f7ed60ea80ac606ccdd1a7c68ebd955
Leandro Monteiro <neofahrenheituk@gmail.com>
2022-09-16 20:09:48 -0300

    Changed the output file to add the date and time instead of an index

--- streamlink_looper.py
+++ streamlink_looper.py
@@ -1,9 +1,9 @@
 import os
 import sys
+import time
 
 class StreamlinkLooper():
     def __init__(self):
-        self.count = 1
         self.arg = sys.argv[1]
 
         self.get_streamer_name()
@@ -15,14 +15,16 @@ class StreamlinkLooper():
         self.streamer_name = arg.split('twitch.tv/')[1].split(' ')[0]
 
     def get_modified_command(self):
-        filename = self.arg.split(' ')[-1]
-        mod_filename = f'{self.count}_{self.streamer_name}.mp4'
-        self.command = f'{self.arg.split("-o")[0]}-o {mod_filename}'
+        t = time.localtime()
+        now = time.strftime('%Y-%m-%d_%H-%M-%S', t)
+        filename = f'{self.streamer_name}_{now}.mp4'
+        self.command = f'{self.arg.split("-o")[0]}-o {filename}'
 
     def start_looping(self):
         while (True):
             self.get_modified_command()
             os.system(f'cmd /c {self.command}')
-            self.count += 1
 
 classObj = StreamlinkLooper()
+
+
